5d6e8effd0aca2cbd0cb541556491be1e6ae0edc
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-12-21 17:33:31 +0530

    commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,12 +1,43 @@
 import User from "../models/user.models";
 import { errorHandler } from "../utils/error";
-
+import bcrypt from 'bcrypt';
 
 const register = async(req, res, next) => {
-    const { email, password } = req.body;
+    const { email, password, name } = req.body;
     try {
 
-    } catch (error) {
 
+        // to check user already exist or not
+        const existinguser = await User.findOne({ email });
+        if (existinguser) {
+            return next(errorHandler(404, 'User alreay exist with thi email'));
+        }
+
+        // to save new user
+        const hashedpassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            name,
+            email,
+            password: hashedpassword,
+
+        });
+
+        await newUser.save();
+
+
+        res.status(201).json({
+            success: true,
+            message: 'User registered successfully',
+            User: {
+
+                email: newUser.email,
+                name: newUser.name,
+                // Excluding the password from the response
+            },
+        })
+
+    } catch (error) {
+        console.log(`register error: ${error}`);
+        return next(errorHandler(500, "Internal server error"));
     }
 }
