e8b24101599cbb823e549da5ce838d567d354fa7
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-12-21 17:16:53 +0530

    Commit

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,12 @@
+import User from "../models/user.models";
+import { errorHandler } from "../utils/error";
+
+
+const register = async(req, res, next) => {
+    const { email, password } = req.body;
+    try {
+
+    } catch (error) {
+
+    }
+}
