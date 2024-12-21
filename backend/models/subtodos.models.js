4930152f030d7ba4b200b26acc2564d6bf33dcc6
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-12-21 13:19:52 +0530

    commit

---
+++ backend/models/subtodos.models.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+
+const subTodoSchema = new mongoose.Schema({
+
+}, { timestamps: true });
+
+
+const SubTodo = mongoose.model('SubTodo', subTodoSchema);
+
+export default SubTodo;
