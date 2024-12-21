15d9ffdcc1a9608fb1e24a8bca4e32548670cb2e
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-12-21 12:43:22 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,34 @@
 import express from 'express';
 import cors from 'cors';
-import env from 'dotenv';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
-
+dotenv.config();
 const password = process.env.password;
 const port = process.env.PORT || 8001;
 
+// mongodb connection 
+const uri = `mongodb+srv://adarshsingh2003v:${password}@cluster0.wd9u3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Mongodb connected'))
+    .catch((error) => console.log(`Mongodb error ${error}`));
+
+
+
+
+// middlewre
+
+app.use(express.json());
+app.use(cors());
+
+
+
 
 app.listen(port, () => {
-    console.log(`Server is  listening on port ${port}`);
+    console.log(`
+Server is listening on port ${ port }
+`);
 })
