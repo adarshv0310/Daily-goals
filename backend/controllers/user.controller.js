dd505ef65896b6871b3b8ec3fd5896c104938e14
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-12-21 18:00:01 +0530

    Commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.models.js";
 import { errorHandler } from "../utils/error.js";
 import bcrypt from 'bcrypt';
-
+import jwt from "jsonwebtoken";
 export const register = async(req, res, next) => {
     const { email, password, name } = req.body;
     try {
@@ -39,5 +39,27 @@ export const register = async(req, res, next) => {
     } catch (error) {
         console.log(`register error: ${error}`);
         return next(errorHandler(500, "Internal server error"));
+    }
+}
+
+
+export const login = async(req, res, next) => {
+    const { email, password } = req.body;
+    try {
+
+        const existinguser = await User.findOne({ email });
+        if (!existinguser) {
+            return next(errorHandler(401, 'User does not exist '));
+        }
+
+        const validpassword = await bcrypt.compare(password, existinguser.password);
+        if (!validpassword) {
+            return next(errorHandler(401, 'Invalid password '));
+        }
+
+
+    } catch (error) {
+        console.log(`Login error${error}`);
+        return next(errorHandler(500, 'Internal server error'));
     }
 }
