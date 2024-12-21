c9ab1790d8a5603574dbc559351d16e54e5a6699
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-12-21 12:24:35 +0530

    Commit

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
-import express from express
+import express from 'express';
+import cors from 'cors';
+import env from 'dotenv';
+
+const app = express();
+
+const password = process.env.password;
+const port = process.env.PORT || 8001;
+
+
+app.listen(port, () => {
+    console.log(`Server is  listening on port ${port}`);
+})
