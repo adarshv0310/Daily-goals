9dad831f04ed21b0c8f819a8bc2b062a367168c1
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-12-29 12:37:45 +0530

    commit

--- backend/controllers/todos.controller.js
+++ backend/controllers/todos.controller.js
@@ -1,2 +1,19 @@
 import Todo from "../models/todos.models";
 import User from "../models/user.models";
+import { errorHandler } from "../utils/error";
+
+
+
+export const getTodos = async(req, res, next) => {
+    try {
+        const userId = req.param.userId;
+        const todos = await Todo.find({ createdBy: userId }).populate('subTodos');
+
+        res.status(200)
+            .json(todos);
+
+    } catch (error) {
+        console.log(`Login error${error.message}`);
+        return next(errorHandler(500, 'Internal server error'));
+    }
+}
