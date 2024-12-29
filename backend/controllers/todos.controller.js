77d588ca6b61a21d38c8c64ffb67e62a00a4d76e
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-12-29 12:48:15 +0530

    commit

--- backend/controllers/todos.controller.js
+++ backend/controllers/todos.controller.js
@@ -17,3 +17,22 @@ export const getTodos = async(req, res, next) => {
         return next(errorHandler(500, 'Internal server error'));
     }
 }
+
+
+export const getTodobyId = async(req, res, next) => {
+    try {
+
+        const todoId = req.param.TodoId;
+        const todo = await Todo.findById(todoId).populate('subTodos');
+
+        if (!todo) {
+            return next(errorHandler(404, 'Todo not found '));
+        }
+        res.status(200).json(todo);
+
+
+    } catch (error) {
+        console.log(`Login error${error.message}`);
+        return next(errorHandler(500, 'Internal server error'));
+    }
+}
